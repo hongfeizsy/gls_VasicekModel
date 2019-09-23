de18920557983a735c35f10b6dcfaa5e0dfdfb8c
hongfeizsy <hongfeizsy@gmail.com>
2019-09-23 16:46:51 +0200

    Create 'MCFunctions_MeanReversion.h' file.

--- GSL/GSL/GSL.cpp
+++ GSL/GSL/GSL.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <boost/timer.hpp>
+
+void quickTest() {
+	unsigned long seed = 123;
+	double kappa = 0.05, theta = 80, sigma = 0.15, deltaT = 1.0 / 252, startTime = 0, endTime = 300;
+	double Y0 = 109;
+
+
+}
+
 
 int main()
 {
-	std::cout << "Hello World!\n" << std::endl;
+	unsigned long seed = 123;
+
+	return 0;
 }
---
+++ GSL/GSL/MCFunctions_MeanReversion.h
@@ -0,0 +1,6 @@
+#include <random>
+#include <gsl/gsl_statistics_double.h>
+#include <gsl/gsl_multimin.h>
+#include <gsl/gsl_blas.h>
+
+
